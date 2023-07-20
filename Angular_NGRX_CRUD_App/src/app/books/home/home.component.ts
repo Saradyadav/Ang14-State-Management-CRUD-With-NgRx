798947fac8ae9ef7c0a52ad99b64e608a50e0af2
saradyadav <sarad.ky@samsung.com>
2023-07-20 17:15:47 +0530

    Implement Read Operation

--- Angular_NGRX_CRUD_App/src/app/books/home/home.component.ts
+++ Angular_NGRX_CRUD_App/src/app/books/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit{
+
+  /**
+   *
+   */
+  constructor(private store : Store) {
+    
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- Angular_NGRX_CRUD_App/src/app/books/store/books.reducer.ts
+++ Angular_NGRX_CRUD_App/src/app/books/store/books.reducer.ts
@@ -1,2 +1,8 @@
-export class BooksReducer {
-}
+import { createReducer } from "@ngrx/store";
+import { Book } from "./book";
+
+export const initialState : ReadonlyArray<Book>=[];
+
+export const bookReducer = createReducer(
+    initialState 
+)
