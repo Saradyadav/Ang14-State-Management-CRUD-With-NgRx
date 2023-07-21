962232ad033e1c54edd687da98eccd2d1efc0dd1
saradyadav <sarad.ky@samsung.com>
2023-07-21 22:16:37 +0530

    generate 'book.service.ts' and  Get API call

--- Angular_NGRX_CRUD_App/src/app/books/store/books.effects.ts
+++ Angular_NGRX_CRUD_App/src/app/books/store/books.effects.ts
@@ -1,2 +1,11 @@
+import { Injectable } from "@angular/core";
+import { Actions,createEffect } from "@ngrx/effects";
+import { BooksService } from "../books.service";
+
+@Injectable()
 export class BooksEffects {
+    constructor(private action$:Actions,
+    private bookService:BooksService){}
+
+    loadAllBooks$ = createEffect
 }
